0b8c58c2eb84fb51a5ca30ad84b31ac81c4c825c
Saimplice Ruud Christopher <saimpliceudemy@gmail.com>
2021-07-27 10:14:34 -0700

    Add files via upload

---
+++ GameManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class GameManager : MonoBehaviour
+{   
+    public Animator Fader;
+    public float TimeSwitchScene;
+    public void Reload(){
+        StartCoroutine(ReloadScene());
+    }
+    IEnumerator ReloadScene(){
+        Fader.Play("FadeIn");
+        yield return new WaitForSeconds(TimeSwitchScene);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
